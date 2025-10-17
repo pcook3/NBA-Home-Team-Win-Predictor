8883255298c5bcd3cefd7d1e10b18817fe741ac0
pcook3 <philipordrew@gmail.com>
2025-10-17 17:24:39 -0400

    feature engineering and features+target creation

--- cleanse.py
+++ cleanse.py
@@ -20,7 +20,10 @@ df = df[(df['gameDate_int'] >= start_date) & (df['gameDate_int'] <= end_date)]
 
 columns_to_drop = ['gameDate_y', 'hometeamId_y', 'awayteamId_y', 'gameLabel_y', 
                    'gameSubLabel_y', 'seriesGameNumber_y', 'gameLabel_x', 'gameSubLabel_x', 
-                   'seriesGameNumber_x', 'gameDay', 'gameSubtype', 'seriesText', 'coachId']
+                   'seriesGameNumber_x', 'gameDay', 'gameSubtype', 'seriesText', 'coachId', 
+                   'teamScore', 'opponentScore', 'teamName', 'teamId', 'opponentTeamCity', 
+                   'opponentTeamName', 'opponentTeamId', 'opponentScore', 'winner', 
+                   'teamCity']
 df = df.drop(columns=columns_to_drop)
 
 df = df.rename(columns={
@@ -32,8 +35,52 @@ df = df.rename(columns={
 #drop the duplicate gameDate column (keeping the integer type column)
 df = df.drop(columns=['gameDate'])
 
+#combine home+away columns into one row for easy reference
+home_df = df[df['home'] == 1].copy()
+away_df = df[df['home'] == 0].copy()
+
+df = home_df.merge(
+    away_df,
+    on='gameId',
+    suffixes=('_home', '_away')
+)
+
+
+#feature engineering
+df['fgpDiff'] = df['fieldGoalsPercentage_home'] - df['fieldGoalsPercentage_away']
+df['3ppDiff'] = df['threePointersPercentage_home'] - df['threePointersPercentage_away']
+df['reboundDiff'] = df['reboundsTotal_home'] - df['reboundsTotal_away']
+df['turnoverDiff'] = df['turnovers_home'] - df['turnovers_away']
+
+#creating a column for days since last game
+df['gameDateTimeEst_home'] = pd.to_datetime(df['gameDateTimeEst_home'])
+df = df.sort_values(by=['gameDateTimeEst_home'])
+
+#for home
+df['days_since_last_home_game'] = (
+    df.groupby('hometeamId_home')['gameDateTimeEst_home']
+      .diff()
+      .dt.days
+      .fillna(0)
+)
+
+#for away
+df['days_since_last_away_game'] = (
+    df.groupby('awayteamId_away')['gameDateTimeEst_home']
+      .diff()
+      .dt.days
+      .fillna(0)
+)
+
+#more feature engineering
+df['rest_diff'] = df['days_since_last_home_game'] = df['days_since_last_away_game']
+df['back_to_back_home'] = (df['days_since_last_home_game'] == 1).astype(int)
+df['back_to_back_away'] = (df['days_since_last_away_game'] == 1).astype(int)
+
+
+
 
 #convert the merged and cleansed data frame into a CSV file (commented out the line below)
-# df.to_csv('merged_filtered_games.csv', index=False)
+#df.to_csv('merged_filtered_games.csv', index=False)
 
 print(df.columns.to_list())
